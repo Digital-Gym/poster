d78ba0ff7005b1c6476cd9e4d344ec176a113231
muminovbob <bobomurodm2004@gmail.com>
2024-02-05 21:15:35 +0500

    hello world!

---
+++ src/components/PostCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import HeartIcon from "../components/icons/Heart.vue"
+import ViewIcon from "../components/icons/Views.vue"
+
+defineProps(['post'])
+</script>
+
+<template>
+    <div class="single-post">
+        <img v-if="post.image" :src="post.image">
+        <p v-if="post.caption" class="caption-text">{{ post.caption }}</p>
+        <div class="post-info">
+            <div class="likes">
+                <HeartIcon />
+                <p>{{ post.likes }}</p>
+            </div>
+            <div class="views">
+                <ViewIcon />
+                <p>{{ post.views }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.single-post{
+    border: solid 1px red;
+}
+
+img{
+    width: 70%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import PostCard from '../components/PostCard.vue'
+import sourceData from "../../posts.json"
+
+
+</script>
+
+<template>
+    <h1>Home</h1>
+    <div class="main-content">
+        <PostCard 
+            v-for="post in sourceData"
+            :post=post
+        />
+    </div>
+</template>
+
+<style scoped>
+.main-content{
+    width: 75%;
+}
+</style>
