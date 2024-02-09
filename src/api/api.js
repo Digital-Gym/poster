fe251a33cee76bfdd50befca6e303d18fb4f9865
muminovbob <bobomurodm2004@gmail.com>
2024-02-09 21:46:40 +0500

    fix: remove redundant appends

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,14 @@ import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
 
 axios.interceptors.request.use((config) => {
-    const authStore = useAuthStore()
-    let params = new URLSearchParams()
-    params.append('auth', authStore.userInfo.token)
-    
-    config.params = params
+    const url = config.url
+
+    if(!url.includes('signUp') && !url.includes('signInWithPassword')){
+        const authStore = useAuthStore()
+        let params = new URLSearchParams()
+        params.append('auth', authStore.userInfo.token)
+        
+        config.params = params
+    }
     return config
 })
